7fd7aab554c2c2cbfb875833780cb5744facaa03
kinduff <kinduff@protonmail.com>
2025-01-30 11:10:46 -0600

    Refactors and simplifies

--- extension.js
+++ extension.js
@@ -1,101 +1,97 @@
-// This extensions shows EUR to USD convertion on Gnome panel.
+// This extension shows USD to MXN conversion on Gnome panel.
 // Copyright (C) 2024 vezza
 // See LICENSE file
 
 'use strict';
 
-import St from 'gi://St'
-import Clutter from 'gi://Clutter'
-import Soup from 'gi://Soup'
-import GLib from 'gi://GLib'
-
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+import Soup from 'gi://Soup';
+import GLib from 'gi://GLib';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 let panelButton;
-let panelButtonText;
 let session;
-let dollarQuotation;
 let sourceId = null;
 
-// Handle Requests API Dollar
-async function handle_request_api() {
+async function fetchDollarQuotation() {
+  if (!session) {
+    session = new Soup.Session({ timeout: 10 });
+  }
+
+  const message = Soup.Message.new_from_encoded_form(
+    "GET", "https://economia.awesomeapi.com.br/last/USD-MXN", Soup.form_encode_hash({})
+  );
+
+  try {
+    const text = await session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null);
+    return new TextDecoder().decode(text.get_data());
+  } catch (error) {
+    console.error(`Error fetching dollar quotation: ${error}`);
+  } finally {
+    session.abort();
+  }
+
+  return null;
+}
+
+function updatePanelText(quotation) {
+  let displayText = quotation
+    ? `1 USD = ${quotation} MXN`
+    : "";
+
+  const panelButtonText = new St.Label({
+    text: displayText,
+    y_align: Clutter.ActorAlign.CENTER,
+  });
+
+  panelButton.set_child(panelButtonText);
+}
+
+async function handleRequestApi() {
+  const response = await fetchDollarQuotation();
+
+  if (response) {
     try {
-        // Create a new Soup Session
-        if (!session) {
-            session = new Soup.Session({ timeout: 10 });
-        }
-
-        // Create body of Soup request
-        let message = Soup.Message.new_from_encoded_form(
-            "GET", "https://economia.awesomeapi.com.br/last/USD-MXN", Soup.form_encode_hash({}));
-
-        // Send Soup request to API Server
-        await session.send_and_read_async(message, GLib.PRIORITY_DEFAULT, null, (_, r0) => {
-            let text = session.send_and_read_finish(r0);
-            let response = new TextDecoder().decode(text.get_data());
-            const body_response = JSON.parse(response);
-
-            // Get the value of Euro Quotation
-            dollarQuotation = body_response["USDMXN"]["bid"];
-            dollarQuotation = dollarQuotation.split(".");
-            dollarQuotation = dollarQuotation[0] + "," + dollarQuotation[1].substring(0, 4);
-
-            // Sext text in Widget
-            panelButtonText = new St.Label({
-                style_class: "cPanelText",
-                text: "(1 USD = " + dollarQuotation + " MXN)",
-                y_align: Clutter.ActorAlign.CENTER,
-            });
-            panelButton.set_child(panelButtonText);
-
-            // Finish Soup Session
-            session.abort();
-            text = undefined;
-            response = undefined;
-        });
+      const bodyResponse = JSON.parse(response);
+      let dollarQuotation = bodyResponse["USDMXN"]["bid"];
+      dollarQuotation = dollarQuotation.substring(0, 5);
+      updatePanelText(dollarQuotation);
     } catch (error) {
-        console.error(`Traceback Error in [handle_request_api]: ${error}`);
-        panelButtonText = new St.Label({
-            text: "(1 USD = " + _dollarQuotation + ")" + " * ",
-            y_align: Clutter.ActorAlign.CENTER,
-        });
-        panelButton.set_child(panelButtonText);
-        session.abort();
+      console.error(`Error parsing dollar quotation: ${error}`);
+      updatePanelText(null);
     }
+  } else {
+    updatePanelText(null);
+  }
 }
 
 export default class Eurusd {
-    enable() {
-        panelButton = new St.Bin({
-            style_class: "panel-button",
-        });
+  enable() {
+    panelButton = new St.Bin({ style_class: "panel-button" });
+    handleRequestApi();
+    Main.panel._centerBox.insert_child_at_index(panelButton, 0);
+    sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
+      handleRequestApi();
+      return GLib.SOURCE_CONTINUE;
+    });
+  }
 
-        handle_request_api();
-        Main.panel._centerBox.insert_child_at_index(panelButton, 0);
-        sourceId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 30, () => {
-            handle_request_api();
-            return GLib.SOURCE_CONTINUE;
-        });
+  disable() {
+    if (panelButton) {
+      Main.panel._centerBox.remove_child(panelButton);
+      panelButton.destroy();
+      panelButton = null;
     }
 
-    disable() {
-        Main.panel._centerBox.remove_child(panelButton);
-
-        if (panelButton) {
-            panelButton.destroy();
-            panelButton = null;
-        }
-        panelButtonText = null;
-        dollarQuotation = null;
-
-        if (sourceId) {
-            GLib.Source.remove(sourceId);
-            sourceId = null;
-        }
-
-        if (session) {
-            session.abort(session);
-            session = null;
-        }
+    if (sourceId) {
+      GLib.Source.remove(sourceId);
+      sourceId = null;
     }
+
+    if (session) {
+      session.abort();
+      session = null;
+    }
+  }
 }
